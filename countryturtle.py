b48b5c36ccf046cde341755f6050906ff3f2d445
LucasSpektralInc <lucas.amorimsegantin.885@accesscreative.ac.uk>
2023-11-01 13:16:53 +0000

    CountryTurtle

--- countryturtle.py
+++ countryturtle.py
@@ -1,8 +1,17 @@
 import turtle
 
+whole_world = "CountryGuesser/images/World_map_4500px.gif"
+
+
 t = turtle.Turtle()
+window = turtle.Screen()
+window.setup(1920, 1080)
+
 
 class World:
     def DrawWorld():
-        t.forward(100)
-        t.right(90)
+        window.addshape(whole_world)
+        turtle.shape(whole_world)
+
+World.DrawWorld()
+turtle.mainloop()

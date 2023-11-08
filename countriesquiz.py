dc68455edfc5004e68afed5c3ac3d9c5e8720dc5
LucasSpektralInc <lucas.amorimsegantin.885@accesscreative.ac.uk>
2023-11-08 11:45:05 +0000

    UI n Quiz

--- countriesquiz.py
+++ countriesquiz.py
@@ -5,23 +5,51 @@ from ui import *
 Functions.Restart()
 Welcome.beginning()
 
+a = int(input(" "))
+if a == 1:
+    for i in range(0, 195):
+        score = 0
+        choice = str(input("Enter a country name: "))
+        choice = choice.lower()
+        if choice == "quit" or choice == "stop":
+            quit()
+        elif choice == "restart":
+            print("Game Will Restart")
+            Functions.Restart()
+        elif choice in removedCountries:
+            print("You have already said this country!")
+            print(f"Your score is still {len(removedCountries)-1}")
+        elif choice in countries:
+            print(choice + " is in the world!")
+            removedCountries.append(choice)
+            countries.remove(choice)
+            print(f"Your score is {len(removedCountries)}")
+        else:
+            print(choice + " is not in the world!")
 
-for i in range(0, 195):
-    score = 0
-    choice = str(input("Enter a country name: "))
-    choice = choice.lower()
-    if choice == "quit" or choice == "stop":
-        quit()
-    elif choice == "restart":
-        print("Game Will Restart")
+elif a == 2:
+    b = int(input("""                  Press 1 for Europe, 2 for Asia, 3 for Oceania, 4 for North America,
+                  5 for South America and 6 for Africa(7 for a Bonus continent)"""))
+    if b == 1:
         Functions.Restart()
-    elif choice in removedCountries:
-        print("You have already said this country!")
-        print(f"Your score is still {len(removedCountries)-1}")
-    elif choice in countries:
-        print(choice + " is in the world!")
-        removedCountries.append(choice)
-        countries.remove(choice)
-        print(f"Your score is {len(removedCountries)}")
-    else:
-        print(choice + " is not in the world!")
+        for i in range(0, len(countriesInEurope)):
+            score = 0
+            choice = str(input("Enter the name of a country in Europe"))
+            choice = choice.lower()
+            if choice == "quit" or choice == "stop":
+                quit()
+            elif choice == "restart":
+                print("Game Will Restart")
+                Functions.Restart()
+            elif choice in removedCountries:
+                print("You have already said this country!")
+                print(f"Your score is still {len(removedCountries)-1}")
+            elif choice in countries:
+                print(choice + " is in Europe!")
+                removedCountries.append(choice)
+                countries.remove(choice)
+                print(f"Your score is {len(removedCountries)} in European countries")
+            else:
+                print(choice + " is not in Europe!")
+else:
+    print("Test")
--- ui.py
+++ ui.py
@@ -3,7 +3,13 @@ class Welcome:
         a = """
             +----------------------+
             | WELCOME TO OUR APP!  |
-            |  SELECT AN OPTION    |
+            |  SELECT AN OPTION:   |
             +----------------------+
+
+            If you would like to play the whole World, press 1
+
+            If you want to play by continents, play 2
+
+            Type "quit" to exit the app
             """
         print(a)
